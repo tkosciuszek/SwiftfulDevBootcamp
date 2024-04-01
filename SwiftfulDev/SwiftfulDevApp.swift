c9512cecbc3a5dd0e337777baea8fa7690c9608c
Teddy Kosciuszek <teddy.kosciuszek@ridedott.com>
2024-04-01 12:50:29 +0200

    Initial Commit

---
+++ SwiftfulDev/SwiftfulDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulDevApp.swift
+//  SwiftfulDev
+//
+//  Created by Teddy on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
